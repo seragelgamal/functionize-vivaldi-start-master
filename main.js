213566a1f7f3129c1b375f7a68bc8e412de0a744
seragelgamal <55516965+seragelgamal@users.noreply.github.com>
2020-07-02 13:17:08 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,49 @@
 // Vivald JS Example
 
 // When a Season Button is Clicked
-document.getElementById('summerBtn').addEventListener('click', setSummer);
-document.getElementById('autumnBtn').addEventListener('click', setAutumn);
-document.getElementById('winterBtn').addEventListener('click', setWinter);
-document.getElementById('springBtn').addEventListener('click', setSpring);
+document.getElementById('summerBtn').addEventListener('click', clickHandler);
+document.getElementById('autumnBtn').addEventListener('click', clickHandler);
+document.getElementById('winterBtn').addEventListener('click', clickHandler);
+document.getElementById('springBtn').addEventListener('click', clickHandler);
 
 
 // Event Functions
+function clickHandler(e) {
+    // console.log(e.target.id);
+    if (e.target.id == 'summerBtn') {
+        changeSeason('summer','#1BA848');
+    } else if (e.target.id == 'autumnBtn') {
+        changeSeason('autumn','#FE6732');
+    } else if (e.target.id == 'winterBtn') {
+        changeSeason('winter','#1C64B9');
+    } else {
+        changeSeason('spring','#0E94D1');
+    }
+}
+
+function changeSeason(season, colour) {
+    // - change concerto text
+    document.getElementById('season-text').innerHTML = season;
+    
+    // - change main image
+    document.getElementById('main-img').src = 'images/'+season+'.jpg';
+    
+    // - change page background color
+    document.body.style.backgroundColor = colour;
+    
+    // - change audio source
+    document.getElementById('song').src = 'songs/vivaldi-'+season+'.mp3';
+    
+    // - remove active class from all buttons
+    document.getElementById('springBtn').classList.remove('activeBtn');
+    document.getElementById('summerBtn').classList.remove('activeBtn');
+    document.getElementById('autumnBtn').classList.remove('activeBtn');
+    document.getElementById('winterBtn').classList.remove('activeBtn');
+
+    // - add active class to clicked button
+    document.getElementById(season+'Btn').classList.add('activeBtn');
+}
+
 function setSummer() {
     // - change concerto text
     document.getElementById('season-text').innerHTML = 'summer';
